e3ed22ca9f74280c322e6d6243240af5c50e59c7
Ryan Burns <ryankburns@gmail.com>
2021-08-13 16:27:31 -0700

    commit tree d6b21f527128691e7598cfe6d4945280fd9ebfbc

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
-  render() {
-    return(
-      <div>
-        Band Component
-      </div>
-    );
-  }
-};
+handleOnClick = () => {
+  return this.props.deleteBand(this.props.band.id)
+}
+
+render() {
+  return(
+    <div>
+      <li>{this.props.band.name}</li>
+      <button onClick={this.handleOnClick}>
+        Delete
+      </button>
+    </div>
+  )
+}
+}
 
 export default Band;
